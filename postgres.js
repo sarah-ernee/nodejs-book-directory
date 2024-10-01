6f47c89ffed891010a32cd37a6a4ee08810b200f
Sarah Ng <93563644+sarah-ernee@users.noreply.github.com>
2024-10-01 21:49:52 +0800

    added postgres into implementation code

--- postgres.js
+++ postgres.js
@@ -74,8 +74,9 @@ app.post("/users", async (req, res) => {
     ]);
     res.status(200).json({ message: "Successfully added new user to sql" });
   } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
+    res.status(500).json({
+      message: `Error occurred in POST user function: ${err}`,
+    });
   }
 });
 
@@ -88,21 +89,52 @@ app.put("/users/:id", async (req, res) => {
     return res.status(400).json({ message: "User ID is a required field" });
   }
 
-  try {
-    const user = await pool.query("SELECT * FROM users WHERE user = $1", [id]);
+  if (!name || !email) {
+    return res
+      .status(400)
+      .json({ message: "Name and email are required fields" });
+  }
 
-    if (user.rowCount === 0) {
-      return res
-        .status(400)
-        .json({ message: `User with ID ${id} does not exist in database` });
+  try {
+    const updateUser = await pool.query(
+      "UPDATE users SET name = $ 1, email = $2 WHERE id = $3",
+      [name, email, id]
+    );
+
+    if (updateUser.rowCount === 0) {
+      return res.status(400).json({
+        message: `User with ID ${id} was not found and could not be updated`,
+      });
     }
 
-
-
-    res.status(200).json(user.rows);
+    res.status(200).json({ message: `Succesfully updated user with ID ${id}` });
   } catch (err) {
     res.status(500).json({
-      message: `Error occurred in GET specific user function: ${err}`,
+      message: `Error occurred in PUT user function: ${err}`,
+    });
+  }
+});
+
+// Remove a user
+app.delete("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleteUser = await pool.query("DELETE FROM users WHERE id = $1", [
+      id,
+    ]);
+
+    if (deleteUser.rows.length === 0) {
+      return res.status(404).json({
+        message: `User with ID ${id} was not found and could not be deleted`,
+      });
+    }
+
+    res
+      .status(200)
+      .json({ message: `Successfully deleted user with ID ${id}` });
+  } catch (err) {
+    res.status(500).json({
+      message: `Error occurred in DELETE  user function: ${err}`,
     });
   }
 });
